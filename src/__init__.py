099cacf97bd016868d15768bbe6ef2ff6971a354
Jethro Guce <gucejethro@gmail.com>
2022-03-08 23:20:01 +0800

    Add notes

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+# Intentionally forked and untouched any of this
+# don't want to steal credit of the original author
+# If I would to write a solution might end up close to this source
+# though my solution might not have as extensive documentation as this repo contains given the time
+
+# not to mock you with the exam or something
+# but why re-implement something that has been solved already
+# could be a good learning process though
+# or an oppotunity to show my skill or something
+
+# I use github properly and google
+# this is a sample implementation of a binary tree which is commonly used as code interview exam
+
+# not sure if you also have time to write own test.
